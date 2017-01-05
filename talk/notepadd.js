0eb9309bd22dfde3fa3948ffbf517a0e77400bd2
yl000github <450887644@qq.com>
2017-01-05 15:08:13 +0800

    oo

---
+++ talk/notepadd.js
@@ -0,0 +1,19 @@
+load("/lib/common.js");
+load("/lib/function.js");
+load("/lib/DBUtil.js");
+load("/lib/HttpUtil.js");
+load("/lib/JRobotUtil.js");
+Action(function(request){
+//	checkParamsE(request, ["question"]);
+	var line="hello yang you are best!";
+	JRobotUtil.shortCut("switchPage");
+	JRobotUtil.mouseMove(0.5,0.5,true);
+	for(var i=0;i<line.length;i++){
+		var c=line.charAt(i);
+		JRobotUtil.inputText(c);
+		JRobotUtil.sleep(300);
+	}
+	$_response_$={
+			errorCode:0
+	}
+})
